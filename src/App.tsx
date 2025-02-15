98defde2563b99568f852c6e0c8cc7336cf645be
Matheus Ribeiro <matheus.tex02@gmail.com>
2025-02-15 14:24:29 -0300

    navigation filter style created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import Header from './components/header'
+import arrowIcon from './assets/arrow_icon.png';
+import { useState } from 'react';
 // border-2 border-red-500
+type Filter = 'todos' | 'camisetas' | 'canecas';
+
 function App() {
-  
+  const [activeFilter, setActiveFilter] = useState('todos'); // valor inicial, por exemplo, 'todos'
+
+  // Função para aplicar o filtro
+  const handleFilterClick = (filter: Filter) => {
+    setActiveFilter(filter);
+  };
   return (
-    <div className="bg-customGray p-4 h-screen p-0 ">
+    <div className="bg-customGray h-screen p-0  ">
       <Header/>
-      
+    <div className=' w-[1200px]   m-auto'>
+
+      <div className='flex h-[70px] items-center justify-between'>
+      <nav className="flex items-center space-x-4 p-4">
+      <button
+        onClick={() => handleFilterClick('todos')}
+        className={`text-gray-500 ${activeFilter === 'todos' ? 'underline decoration-orange-500 decoration-2' : ''}`}
+      >
+        Todos os Produtos
+      </button>
+      <button
+        onClick={() => handleFilterClick('camisetas')}
+        className={`text-gray-500 ${activeFilter === 'camisetas' ? 'underline decoration-orange-500 decoration-2' : ''}`}
+      >
+        Camisetas
+      </button>
+      <button
+        onClick={() => handleFilterClick('canecas')}
+        className={`text-gray-500 ${activeFilter === 'canecas' ? 'underline decoration-orange-500 decoration-2' : ''}`}
+      >
+        Canecas
+      </button>
+    </nav>
+        <div className=' mx-7 text-gray-500 '>        
+           <button className='flex items-center justify-center'>Organizar por <img src={arrowIcon} className='mx-2 h-1'/></button>
+
+        </div>
+
+      </div>
+    
+    </div>
   </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,19 +7,19 @@ function Header() {
   return (
     <div>
          <div className="flex justify-center items-center h-20  text-black bg-white ">
-          <div className='h-full w-[1200px] flex justify-between items-center px-20'>
+          <div className='h-full w-[1200px] flex justify-between items-center ph-20'>
 
-            <h1 className='font-saira-stencil text-4xl p-4 text-black text-gray-500 mr-10'>capputeeno</h1>
+            <h1 className='font-saira-stencil text-4xl p-4 text-black text-gray-500'>capputeeno</h1>
 
             <div className='flex justify-center items-center  p-4'>
               <div className='bg-secondGray rounded-lg w-[350px] cursor-text flex justify-center items-center'> 
                 
                 <input type="text" className='border-0  p-2 outline-none w-[300px] bg-transparent text-sm' placeholder='Procurando por algo específico?'/>
-                <img src={searchIcon} className=' h-6'/>
+                <img src={searchIcon} className=' h-5'/>
               </div>
               <button className="relative w-[40px] h-[40px] mx-3">
-                <img src={storeIcon} className="w-[30px] " />
-                <span className="absolute bottom-[-1px] right-[8px] w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs">
+                <img src={storeIcon} className="w-[25px] " />
+                <span className="absolute bottom-[1px] right-[10px] w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs">
                   {shopQtd}
                 </span>
               </button>
